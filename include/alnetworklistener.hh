f0de41ca1cc0a4def3c22ab290b633c521daa8d4
Simon Jagoe <simon@simonjagoe.com>
2007-09-10 13:08:54 +0200

    added header files to be used for interface definitions

--- include/alnetworklistener.hh
+++ include/alnetworklistener.hh
@@ -1,6 +1,14 @@
 #ifndef __ALNETWORKLISTENER_HH__
 #define __ALNETWORKLISTENER_HH__
 
+class RawPacket;
+class InterfaceRoute;
 
+class ALNetworkListener
+{
+    public:
+        virtual void packetReceived( RawPacket& packet,
+            InterfaceRoute& interfaces ) = 0;
+};
 
 #endif
---
+++ include/alstatisticslistener.hh
@@ -0,0 +1,9 @@
+#ifndef __ALSTATISTICSLISTENER_HH__
+#define __ALSTATISTICSLISTENER_HH__
+
+class ALStatisticsListener
+{
+
+};
+
+#endif
