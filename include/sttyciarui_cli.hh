ad61c5fd5e703183bab7cbde507cc6365eea1138
Simon Jagoe <simon@simonjagoe.com>
2007-09-16 16:53:20 +0200

    created basis class of the CLI

--- include/sttyciarui_cli.hh
+++ include/sttyciarui_cli.hh
@@ -1,6 +1,13 @@
 #ifndef __STTYCIARUI_CLI_HH__
 #define __STTYCIARUI_CLI_HH__
 
+#include "sttyciarui_common.hh"
 
+class SttyciarCLI: public SttyciarUI
+{
+    Q_OBJECT
+    public:
+        SttyciarCLI();
+};
 
 #endif
