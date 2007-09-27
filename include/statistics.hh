39f063b906fc2b620181592b7a979bb617562ef5
Doron Horwitz <doronhorwitz@gmail.com>
2007-09-27 14:33:59 +0200

    starting statistics layer

--- include/statistics.hh
+++ include/statistics.hh
@@ -9,7 +9,8 @@ class Statistics
     Q_OBJECT
     public:
         Statistics();
-        double getPercentage(shared_ptr<Device> source, shared_ptr<Device> destination);
+        double calculateTrafficPercentage(QMap<shared_ptr
+        double getTrafficPercentage(shared_ptr<Device> source, shared_ptr<Device> destination);
 
     private:
         QMap<shared_ptr<Device>,QMap<shared_ptr<Device>,double> > _percentageTraffic;
