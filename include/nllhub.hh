00aa70fd11742c4ecb9c68de31196cdd0c753f55
Simon Jagoe <simon@simonjagoe.com>
2007-09-28 11:02:09 +0200

    commented nllhub.hh

--- include/nllhub.hh
+++ include/nllhub.hh
@@ -17,22 +17,35 @@ using boost::shared_ptr;
 class AbstractionLayer;
 class RawPacket;
 
+/*!
+Subclass of NetworkLogicLayer providing a basic implementation of a Hub.
+
+\author Simon Jagoe
+*/
 class NLLHub: public NetworkLogicLayer
 {
     public:
+        /*!
+        Empty constructor to initialise the NetworkLogicLayer
+        */
         NLLHub()
             : NetworkLogicLayer()
         {
-            c1 = 0;
-            c2 = 0;
+//            c1 = 0;
+//            c2 = 0;
         };
 
     protected:
+        /*!
+        Overridden routPacket() method. \see NetworkLogicLayer.
+
+        This provides the logic necessary for a Hub implementation.
+        */
         void routePacket( shared_ptr<RawPacket>& packet );
 
-    private:
-        long long c1;
-        long long c2;
+//    private:
+//        long long c1;
+//        long long c2;
 };
 
 #endif
