7d6db5726179aef47e90a241f8cc8df3d7f2def7
Simon Jagoe <simon@simonjagoe.com>
2007-09-29 17:57:13 +0200

    commented sttyciarrunner.hh

--- include/sttyciarrunner.hh
+++ include/sttyciarrunner.hh
@@ -15,10 +15,22 @@ using boost::scoped_ptr;
 class AbstractionLayer;
 class NetworkLogicLayer;
 
+/*!
+Main class that creates all objects, initialises them, connects them together,
+and passes messages between them to provide the functionality of the system.
+This class is simply instantiated in the main() function to start the
+application (alongside a QApplication, of course).
+
+\author Simon Jagoe
+*/
 class SttyciarRunner: public QObject
 {
     Q_OBJECT
     public:
+        /*!
+        Constructor to create the user interface and abstraction layer, and
+        perform initialisation tasks.
+        */
         SttyciarRunner();
 
     private:
@@ -39,18 +51,57 @@ class SttyciarRunner: public QObject
         shared_ptr<NetworkLogicLayer> _networkLogicLayer; //! The NetworkLogicLayer object
 
     private slots:
+        /*!
+        Slot signalled by the UI to start Sttyciar with a set of Devices,
+        and to act like a particular network system.
+
+        \param deviceType The network System to emulate.
+        \param devices The Devices to bind to the system.
+        */
         void startSttyciar(QString deviceType, shared_ptr<QStringList> devices);
+
+        /*!
+        Slot signalled by the UI to stop Sttyciar and clean up back to the
+        initial state.
+        */
         void stopSttyciar();
+
+        /*!
+        Slot signalled by the UI to stop Sttyciar and exit.
+        */
         void exitSttyciar();
 
+        /*!
+        Slot signalled by the NLLUpdateTimer so that the SttyciarRunner can
+        signal the NLL to update with the number of milliseconds since the last
+        update.
+        */
         void nllUpdateTimeout();
         void statisticsUpdateTimeout();
 
+        /*!
+        Slot signalled by the StatisticsUpdateTimer so that the SttyciarRunner
+        can signal the SL to update with the number of milliseconds since the
+        last update.
+        */
+
     signals:
+        //! Signal to tell the QApplication to exit.
         void exit();
 
+        /*!
+        Signal the NLL to update.
+
+        \param milliseconds The time since the last update in milliseconds.
+        */
         void updateNLL( int milliseconds );
         void updateStatistics( int milliseconds );
+
+        /*!
+        Signal the SL to update.
+
+        \param milliseconds The time since the last update in milliseconds.
+        */
 };
 
 #endif
